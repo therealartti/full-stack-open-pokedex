9f3a8e2f00749cb792bd38c822c06ef23e94a79a
therealartti <107062680+therealartti@users.noreply.github.com>
2023-08-25 15:51:58 +0300

    Trying to fix pipeline

--- app.js
+++ app.js
@@ -6,16 +6,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT,() => {
-  console.log('server started on port', PORT) // eslint-disable-line no-console
-})
-
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
+})
+
+app.listen(PORT,() => {
+  console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })
