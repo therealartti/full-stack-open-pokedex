a391484ae89ad90b305b85a18e4f6e929a136c41
therealartti <107062680+therealartti@users.noreply.github.com>
2023-08-25 14:09:36 +0300

    test

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen(PORT,() => {
   console.log('server started on port 5000') // eslint-disable-line no-console
 })
