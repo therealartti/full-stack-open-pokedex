2ca8a0ccc510616af8fdbcb1510da7ef8ad736dc
therealartti <107062680+therealartti@users.noreply.github.com>
2023-08-25 15:34:40 +0300

    Intentional error

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT,() => {
-  console.log('server started on port 5000') // eslint-disable-line no-console
+  console.log('server started on port', PORT) // eslint-disable-line no-console
 })
 
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('2') // change this string to ensure a new version deployed
 })
